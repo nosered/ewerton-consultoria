bd9e206dbd103c47ccf9fa62f069effaf3dd38c3
ederson <ederson.dimm@gmail.com>
2019-02-25 14:01:51 -0300

    remover tests

--- src/test/java/br/eti/esabreu/ckeditorimgupload/EwertonConsultoriaApplicationTests.java
+++ src/test/java/br/eti/esabreu/ckeditorimgupload/EwertonConsultoriaApplicationTests.java
@@ -1,15 +1,19 @@
 package br.eti.esabreu.ckeditorimgupload;
 
+/*
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+*/
 
+/*
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CkeditorImgUploadApplicationTests {
+*/
+public class EwertonConsultoriaApplicationTests {
 
-	@Test
+	// @Test
 	public void contextLoads() {
 	}
 
